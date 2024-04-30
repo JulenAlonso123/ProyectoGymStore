6b422abd6412c9ce5dacd5b2a2d8fbc5d76df405
BorjaLopez11 <borja.lopezmartinez@cieep.com>
2024-04-30 14:05:58 +0200

    cambio del texto principal

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AreaBannerComponent } from '../area-banner/area-banner.component';
+import { InformacionHomeComponent } from '../informacion-home/informacion-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, AreaBannerComponent],
+  imports: [RouterLink, AreaBannerComponent, InformacionHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
