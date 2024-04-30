36f9dcd9f8513598f8193b90b2ea7eed3e53b617
Pablo Arnau <pabloarlo00@gmail.com>
2024-04-30 13:06:38 +0200

    Pablo arnau ha subido los componentes navbar y area banner

---
+++ src/app/component/area-banner/area-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './area-banner.component.html',
+  styleUrl: './area-banner.component.css'
+})
+export class AreaBannerComponent {
+
+}
