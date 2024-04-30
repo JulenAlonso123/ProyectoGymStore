4511793a535a5e4384b6344b95de01355b3883dd
Pablo Arnau <pabloarlo00@gmail.com>
2024-04-30 13:35:21 +0200

    Pablo arnau ha añadido un componente home, con todas sus rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 
 export const routes: Routes = [
-
+    { path: '', redirectTo: '/home',  pathMatch: 'full'},
+    { path: 'home', title: 'home',  component: HomeComponent },
+    
 ];
