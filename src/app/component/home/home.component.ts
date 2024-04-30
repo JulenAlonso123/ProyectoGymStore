14828da47491fb8be45f86085097699fe0ad95c8
Pablo Arnau <pabloarlo00@gmail.com>
2024-04-30 13:57:17 +0200

    Pablo arnau ha configurado el area-banner component haciendo un carrusel

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AreaBannerComponent } from '../area-banner/area-banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AreaBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
