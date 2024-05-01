d78fc0fa4e6a7a209635766697aed5bb18668fa5
Pablo Arnau <pablo.arnaulopez@cieep.com>
2024-05-01 15:02:19 +0200

    He añadido los productos bestsellers, y he cambiado el texto de la informacion del home

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AreaBannerComponent } from '../area-banner/area-banner.component';
 import { InformacionHomeComponent } from '../informacion-home/informacion-home.component';
+import { BestsellersComponent } from '../bestsellers/bestsellers.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, AreaBannerComponent, InformacionHomeComponent],
+  imports: [RouterLink, AreaBannerComponent, InformacionHomeComponent, BestsellersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
