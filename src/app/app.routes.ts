eeac4138c83ba0142d72866f9e7c787e237dd8fd
julenalonso123 <julen.alonsorodero@cieep.com>
2024-05-02 13:09:21 +0200

    he añadido el componente catalogo y lo he enlazado a la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { CatalogoComponent } from './component/catalogo/catalogo.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home',  pathMatch: 'full'},
     { path: 'home', title: 'home',  component: HomeComponent },
+    { path: 'catalogo', title: 'home',  component: CatalogoComponent },
     
 ];
---
+++ src/app/component/catalogo/catalogo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-catalogo',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.css'
+})
+export class CatalogoComponent {
+
+}
