405564b35977b660c651caed771413f8a4e6bb12
Pablo Arnau <pablo.arnaulopez@cieep.com>
2024-05-02 18:10:29 +0200

    he creado la pagina de contacto, he creado las dos columnnas, la de informacion y la de las imagenes, y debajo he creado el componente del form, he añadido las imagenes de los integrantes tambien, todo enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { CatalogoComponent } from './component/catalogo/catalogo.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home',  pathMatch: 'full'},
     { path: 'home', title: 'home',  component: HomeComponent },
-    { path: 'catalogo', title: 'home',  component: CatalogoComponent },
+    { path: 'catalogo', title: 'catalogo',  component: CatalogoComponent },
+    { path: 'contacto', title: 'contacto',  component: ContactoComponent },
     { path: '**', component: NotFoundComponent },
 ];
