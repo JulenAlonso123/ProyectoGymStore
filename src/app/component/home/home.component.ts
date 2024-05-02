186dbea4a688ddc747edff76153b3248f105685d
julenalonso123 <julen.alonsorodero@cieep.com>
2024-05-02 10:34:15 +0200

    he añadido la seccion de about us

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { AreaBannerComponent } from '../area-banner/area-banner.component';
 import { InformacionHomeComponent } from '../informacion-home/informacion-home.component';
 import { BestsellersComponent } from '../bestsellers/bestsellers.component';
+import { AboutUsComponent } from '../about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, AreaBannerComponent, InformacionHomeComponent, BestsellersComponent],
+  imports: [RouterLink, AreaBannerComponent, InformacionHomeComponent, BestsellersComponent,AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
