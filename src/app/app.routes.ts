243de38f749792a8c0eeda1f15aca877e3a18394
Pablo Arnau <pablo.arnaulopez@cieep.com>
2024-05-02 16:43:51 +0200

    Pablo arnau ha añadido la pagina de error 404 , le ha dado estilo y ha modificado el archivo routes para poder acceder a ella

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { CatalogoComponent } from './component/catalogo/catalogo.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home',  pathMatch: 'full'},
     { path: 'home', title: 'home',  component: HomeComponent },
     { path: 'catalogo', title: 'home',  component: CatalogoComponent },
-    
+    { path: '**', component: NotFoundComponent },
 ];
