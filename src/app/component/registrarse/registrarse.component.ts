36bb6b9d35769fa6516603ffed558c351a6d3068
Pablo Arnau <pabloarlo00@gmail.com>
2024-05-03 11:25:50 +0200

    He añadido la pagina de registrarse, esta configurada y bien linkeada, he añadido una imagen, 0 fallos

--- src/app/component/registrarse/registrarse.component.ts
+++ src/app/component/registrarse/registrarse.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-registrarse',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.css'
 })
