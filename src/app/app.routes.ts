dc2647e7e2b5d86b138b0b3d17bddc88984383d3
Pablo Arnau <pabloarlo00@gmail.com>
2024-05-03 10:45:53 +0200

    He creado el componente inicio de sesion, lo he linkeado, configurado y dado el estilo necesario, he añadido una foto ademas, 0 fallos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './component/navbar/navbar.component';
 import { CatalogoComponent } from './component/catalogo/catalogo.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 import { ContactoComponent } from './component/contacto/contacto.component';
+import { IniciarsesionComponent } from './component/iniciarsesion/iniciarsesion.component';
+import { RegistrarseComponent } from './component/registrarse/registrarse.component';
 
 
 export const routes: Routes = [
@@ -11,5 +13,8 @@ export const routes: Routes = [
     { path: 'home', title: 'home',  component: HomeComponent },
     { path: 'catalogo', title: 'catalogo',  component: CatalogoComponent },
     { path: 'contacto', title: 'contacto',  component: ContactoComponent },
+    { path: 'iniciarsesion', title: 'iniciar sesion',  component: IniciarsesionComponent },
+    { path: 'registrarse', title: 'Registrarse',  component: RegistrarseComponent },
     { path: '**', component: NotFoundComponent },
+    
 ];
