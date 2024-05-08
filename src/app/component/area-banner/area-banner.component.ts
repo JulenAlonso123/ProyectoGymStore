5b1467656d3f77fbc01490ac814d3bb89f89cea3
Pablo Arnau <pabloarlo00@gmail.com>
2024-05-08 13:57:49 +0200

    hemos corregido los fallos que nos ha dicho bertha, faltas de ortografia, links, botones etc...

--- src/app/component/area-banner/area-banner.component.ts
+++ src/app/component/area-banner/area-banner.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-area-banner',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './area-banner.component.html',
   styleUrl: './area-banner.component.css'
 })
